97bc7cd5527cd82acbe23dfe22284d2d45ff6040
林朝涌 <linchaoyong@linchaongdeiMac.lan>
2015-10-13 02:23:38 +0800

    首屏动画完成

--- src/js/base.js
+++ src/js/base.js
@@ -1,1 +1,13 @@
-console.log('base.js is works.');
+function $(selector) {
+	var elements = document.querySelectorAll(selector);
+	return elements.length > 1 ? elements : elements[0]; 
+}
+
+$.on = function (element, evtName, fn, useCapture) {
+	element.addEventListener(evtName, fn, useCapture);
+};
+
+setTimeout(function () {
+	var body = $('body');
+	body.classList.add('loaded');
+}, 1000);
